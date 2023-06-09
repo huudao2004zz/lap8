16964f4561fdd20a297bb59b09cbf7a3c445df53
huudao2004zz <111514495+huudao2004zz@users.noreply.github.com>
2023-06-09 14:51:16 +0700

    Add files via upload

---
+++ car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    class car
+    {
+        public string make;
+        public string model;
+        public string color;
+        public int yearBuild;
+
+
+        public void Start()
+        {
+            System.Console.WriteLine(model + "started");
+        }
+        public void Stop()
+        {
+            System.Console.WriteLine(model + "stoped");
+        }
+    }
+}
